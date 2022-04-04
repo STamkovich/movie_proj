d8dd1d761314a6015775108e6d053d72907e9307
STamkovich <tamkovich1072@gmail.com>
2022-04-05 00:39:59 +0300

    'вывод таблицы в шаблон'

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
-
 
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=10000000)
 
+    def __str__(self):
+        return f'{self.name} - {self.rating}% {self.year}'
