fcd75cbdba4be955ace76763ea6f3df2b9278f4b
STamkovich <tamkovich1072@gmail.com>
2022-04-08 17:58:49 +0300

    'админка'

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -3,15 +3,43 @@ from .models import Movie
 from django.db.models import QuerySet
 
 
+class RatingFilter(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<40', ' Низкий'),
+            ('от 40 до 79', 'Средний'),
+            ('>=80', 'Высокий'),
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<40':
+            return queryset.filter(rating__lt=40)
+        if self.value() == 'от 40 до 79':
+            return queryset.filter(rating__gte=40).filter(rating__lte=79)
+        if self.value() == '>=80':
+            return queryset.filter(rating__gte=80)
+        return queryset
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
+    # fields = ['name', 'rating'] - добавление в базу данных только с этими указанными полями
+    #exclude = ['slug']  # исключение полей в бд
+    # readonly_fields = ['year'] - запрещать изменять поля при добавлении записей в бд
+    prepopulated_fields = {'slug': ('name', )}
     list_display = ['name', 'rating', 'year', 'budget', 'currency', 'slug', 'rating_status']
     list_editable = ['rating', 'year', 'budget', 'slug', 'currency']
-    ordering = ['name', 'rating']
+    ordering = ['-rating', 'name']
     actions = ['set_dollars', 'set_rubles', 'set_euro']
+    search_fields = ['name__startswith']
+    list_filter = ['name', 'currency', RatingFilter]
 
     # list_per_page = сколько должно отображаться объектов на страничке
 
+    # @admin.display(ordering='rating', description='Статус') - для сортировки поля rating_status, и изменении его имени
     @staticmethod
     def rating_status(mov: Movie):
         if mov.rating < 50:
--- movie_app/models.py
+++ movie_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Movie(models.Model):
@@ -13,9 +14,10 @@ class Movie(models.Model):
         ('RUB', 'Rubles'),
     ]
     name = models.CharField(max_length=20)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1),
+                                             MaxValueValidator(100)])
     year = models.IntegerField(null=True, blank=True)
-    budget = models.IntegerField(default=10000000)
+    budget = models.IntegerField(default=10000000, blank=True, validators=[MinValueValidator(1)])
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB)
     slug = models.SlugField(default='', null=False, db_index=True)
 
